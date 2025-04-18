f4d85860b79a1cc7e89402cc803c67135229e9db
Nur Fikri <codingki@gmail.com>
2025-04-18 11:23:11 +0700

    chore: update vite config

--- example/vite/vite.config.ts
+++ example/vite/vite.config.ts
@@ -1,4 +1,4 @@
-import { default as react } from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import nodeStdlibBrowser from "vite-plugin-node-stdlib-browser";
 
