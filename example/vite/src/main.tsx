1e82e3ebb4f60313859ed116c6eb3fd6240cab23
Jordy McNab <jordy@usecapsule.com>
2025-09-02 10:53:48 -0500

    chore: wip

--- example/vite/src/main.tsx
+++ example/vite/src/main.tsx
@@ -5,10 +5,22 @@ import { GrazProvider } from "graz";
 import { cosmoshub } from "graz/chains";
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
+import { ParaGrazConfig } from "@getpara/graz-integration";
 
 import App from "./App";
+import ParaWeb from "@getpara/react-sdk-lite";
 
 const queryClient = new QueryClient();
+
+// Get an API key at https://developer.getpara.com
+// Modal will open with fake key but will not authenticate
+export const para = new ParaWeb("beta_3e185167932bc6cc7ed11c0abed4dfb8");
+
+const paraConfig: ParaGrazConfig = {
+  paraWeb: para!,
+  modalProps: { appName: "MyApp" },
+  queryClient: queryClient,
+};
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
@@ -16,7 +28,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       <GrazProvider
         grazOptions={{
           chains: [cosmoshub],
-
+          paraConfig: paraConfig,
         }}
       >
         <App />
